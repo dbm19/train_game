8e0385f5e5a837974ca687e7e90fc4f2ed2afd30
dbm19 <daniel.b.mau@gmail.com>
2024-04-16 17:25:03 -0400

    Added 4th (orange) train and terminal

--- game.gd
+++ game.gd
@@ -6,19 +6,24 @@ var train_index = 1
 var trains = {
 	"1": 0,
 	"2": 0,
-	"3": 0
+	"3": 0,
+	"4": 0,
 }
 var blue_train = preload("res://blue_train.tscn")
 var yellow_train = preload("res://yellow_train.tscn")
 var purple_train = preload("res://purple_train.tscn")
+var orange_train = preload("res://orange_train.tscn")
 
 func _ready():
+	spawn_rng = rng.randi_range(1, 4)
 	if spawn_rng == 1:
 		trains[train_index] = blue_train.instantiate()
 	elif spawn_rng == 2:
 		trains[train_index] = yellow_train.instantiate()
-	else:
+	elif spawn_rng == 3:
 		trains[train_index] = purple_train.instantiate()
+	else:
+		trains[train_index] = orange_train.instantiate()
 	trains[train_index].position.x = 35
 	trains[train_index].position.y = 0
 	trains[train_index].scale.x = 1
@@ -27,26 +32,20 @@ func _ready():
 		trains[train_index].add_to_group("train_index_1")
 	elif train_index == 2:
 		trains[train_index].add_to_group("train_index_2")
-	else:
+	elif train_index == 3:
 		trains[train_index].add_to_group("train_index_3")
+	else:
+		trains[train_index].add_to_group("train_index_4")
 	self.add_child(trains[train_index])
 
 func _process(delta):
 	pass
 	
 func _on_timer_timeout():
-	if train_index < 3:
+	if train_index < 4:
 		train_index += 1
 	else:
 		train_index = 1
-	
-	if spawn_rng == 1:
-		spawn_rng = rng.randi_range(2.0, 3.0)
-	elif spawn_rng == 2:
-		spawn_rng = rng.randi_range(1.0, 3.0)
-	else:
-		spawn_rng = rng.randi_range(1.0, 2.0)
-		
 	_ready()
 	
 	
--- junction.gd
+++ junction.gd
@@ -37,8 +37,10 @@ func _on_area_entered(area):
 		_change_train_orientation("train_index_1")
 	elif area.is_in_group("train_index_2"):
 		_change_train_orientation("train_index_2") 
-	else:
+	elif area.is_in_group("train_index_3"):
 		_change_train_orientation("train_index_3")
+	else:
+		_change_train_orientation("train_index_4")
 
 func _on_button_pressed():
 	if get_groups()[0] == "down_right":
