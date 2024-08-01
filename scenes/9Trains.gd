fe7fdf6afaea461ac1d59120e0506f1dcf88630d
dbm19 <daniel.b.mau@gmail.com>
2024-08-01 11:37:21 -0400

    Added new difficulty select scene

--- scenes/9Trains.gd
+++ scenes/9Trains.gd
@@ -1,14 +1,16 @@
 extends Button
 
+var rng = RandomNumberGenerator.new()
+var map_index
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/difficulty_select.tscn")
-	
-
+	get_tree().change_scene_to_file("res://scenes/tile_map_5.tscn")
