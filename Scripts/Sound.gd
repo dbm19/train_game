050ad04bde182089578fd20f344d15275a38257f
dbm19 <daniel.b.mau@gmail.com>
2024-08-05 11:17:24 -0400

    Added fail and success sounds

--- Scripts/Sound.gd
+++ Scripts/Sound.gd
@@ -3,12 +3,16 @@ extends Node
 var junctionSound
 var menuSound
 var backgroundChugSound
+var failSound
+var successSound
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	junctionSound = get_node("JunctionSound")
 	menuSound = get_node("MenuSound")
 	backgroundChugSound = get_node("BackgroundChugSound")
+	failSound = get_node("FailSound")
+	successSound = get_node("SuccessSound")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,6 +26,12 @@ func play_menu_sound():
 
 func play_chug_sound():
 	backgroundChugSound.playing = true
+
+func play_fail_sound():
+	failSound.playing = true
+
+func play_success_sound():
+	successSound.playing = true
 
 func _on_background_chug_sound_finished():
 	play_chug_sound()
