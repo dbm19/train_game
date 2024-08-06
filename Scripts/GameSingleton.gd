8b1589e310a6d6f242a5273f585d0eb9963337cf
dbm19 <daniel.b.mau@gmail.com>
2024-08-06 08:43:31 -0400

    Fixed game singleton logic on startup

--- Scripts/GameSingleton.gd
+++ Scripts/GameSingleton.gd
@@ -2,15 +2,20 @@ extends Node2D
 
 var timer
 var score = 0
-var train_count = 15
+var train_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer = get_node("Timer")
+	train_count = 15
+	score = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func start_game():
+	_ready()
 
 func endTimer():
 	timer.start()
