547f11d17cefbe6a496185b264709ef9a3a21717
dbm19 <daniel.b.mau@gmail.com>
2024-08-07 14:15:24 -0400

    Added curved tracks nice

---
+++ scenes/tracks/curved_track.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_area_entered(area):
+	if self.name == "DownLeft":
+		area.go_left()
+	elif self.name == "DownRight":
+		area.go_right()
+	elif self.name == "UpLeft":
+		area.go_left()
+	elif self.name == "UpRight":
+		area.go_right()
+	elif self.name == "LeftDown":
+		area.go_down()
+	elif self.name == "LeftUp":
+		area.go_up()
+	elif self.name == "RightDown":
+		area.go_down()
+	elif self.name == "RightUp":
+		area.go_up()
