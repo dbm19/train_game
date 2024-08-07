ae40e9dd33bf4a9717cf20be979c4484f2239ced
dbm19 <daniel.b.mau@gmail.com>
2024-08-07 11:30:51 -0400

    Added sprites for the rest of the trains

--- Scripts/Train.gd
+++ Scripts/Train.gd
@@ -19,6 +19,22 @@ func go_up():
 		get_node("Sprite2D").texture = load("res://art_assets/yellow_train_up.png")
 	elif get_groups()[0] == "colour_train_purple":
 		get_node("Sprite2D").texture = load("res://art_assets/purple_train_up.png")
+	elif get_groups()[0] == "colour_train_orange":
+		get_node("Sprite2D").texture = load("res://art_assets/orange_train_up.png")
+	elif get_groups()[0] == "colour_train_pink":
+		get_node("Sprite2D").texture = load("res://art_assets/pink_train_up.png")
+	elif get_groups()[0] == "colour_train_silver":
+		get_node("Sprite2D").texture = load("res://art_assets/silver_train_up.png")
+	elif get_groups()[0] == "colour_train_red":
+		get_node("Sprite2D").texture = load("res://art_assets/red_train_up.png")
+	elif get_groups()[0] == "colour_train_green":
+		get_node("Sprite2D").texture = load("res://art_assets/green_train_up.png")
+	elif get_groups()[0] == "colour_train_black":
+		get_node("Sprite2D").texture = load("res://art_assets/black_train_up.png")
+	elif get_groups()[0] == "colour_train_white":
+		get_node("Sprite2D").texture = load("res://art_assets/white_train_up.png")
+	elif get_groups()[0] == "colour_train_brown":
+		get_node("Sprite2D").texture = load("res://art_assets/brown_train_up.png")
 
 func go_down():
 	velocity = Vector2(0, speed)
@@ -28,6 +44,22 @@ func go_down():
 		get_node("Sprite2D").texture = load("res://art_assets/yellow_train_down.png")
 	elif get_groups()[0] == "colour_train_purple":
 		get_node("Sprite2D").texture = load("res://art_assets/purple_train_down.png")
+	elif get_groups()[0] == "colour_train_orange":
+		get_node("Sprite2D").texture = load("res://art_assets/orange_train_down.png")
+	elif get_groups()[0] == "colour_train_pink":
+		get_node("Sprite2D").texture = load("res://art_assets/pink_train_down.png")
+	elif get_groups()[0] == "colour_train_silver":
+		get_node("Sprite2D").texture = load("res://art_assets/silver_train_down.png")
+	elif get_groups()[0] == "colour_train_red":
+		get_node("Sprite2D").texture = load("res://art_assets/red_train_down.png")
+	elif get_groups()[0] == "colour_train_green":
+		get_node("Sprite2D").texture = load("res://art_assets/green_train_down.png")
+	elif get_groups()[0] == "colour_train_black":
+		get_node("Sprite2D").texture = load("res://art_assets/black_train_down.png")
+	elif get_groups()[0] == "colour_train_white":
+		get_node("Sprite2D").texture = load("res://art_assets/white_train_down.png")
+	elif get_groups()[0] == "colour_train_brown":
+		get_node("Sprite2D").texture = load("res://art_assets/brown_train_down.png")
 
 func go_right():
 	velocity = Vector2(speed, 0)
@@ -37,6 +69,22 @@ func go_right():
 		get_node("Sprite2D").texture = load("res://art_assets/yellow_train_right.png")
 	elif get_groups()[0] == "colour_train_purple":
 		get_node("Sprite2D").texture = load("res://art_assets/purple_train_right.png")
+	elif get_groups()[0] == "colour_train_orange":
+		get_node("Sprite2D").texture = load("res://art_assets/orange_train_right.png")
+	elif get_groups()[0] == "colour_train_pink":
+		get_node("Sprite2D").texture = load("res://art_assets/pink_train_right.png")
+	elif get_groups()[0] == "colour_train_silver":
+		get_node("Sprite2D").texture = load("res://art_assets/silver_train_right.png")
+	elif get_groups()[0] == "colour_train_red":
+		get_node("Sprite2D").texture = load("res://art_assets/red_train_right.png")
+	elif get_groups()[0] == "colour_train_green":
+		get_node("Sprite2D").texture = load("res://art_assets/green_train_right.png")
+	elif get_groups()[0] == "colour_train_black":
+		get_node("Sprite2D").texture = load("res://art_assets/black_train_right.png")
+	elif get_groups()[0] == "colour_train_white":
+		get_node("Sprite2D").texture = load("res://art_assets/white_train_right.png")
+	elif get_groups()[0] == "colour_train_brown":
+		get_node("Sprite2D").texture = load("res://art_assets/brown_train_right.png")
 
 func go_left():
 	velocity = Vector2(-speed, 0) 
@@ -46,6 +94,22 @@ func go_left():
 		get_node("Sprite2D").texture = load("res://art_assets/yellow_train_left.png")
 	elif get_groups()[0] == "colour_train_purple":
 		get_node("Sprite2D").texture = load("res://art_assets/purple_train_left.png")
+	elif get_groups()[0] == "colour_train_orange":
+		get_node("Sprite2D").texture = load("res://art_assets/orange_train_left.png")
+	elif get_groups()[0] == "colour_train_pink":
+		get_node("Sprite2D").texture = load("res://art_assets/pink_train_left.png")
+	elif get_groups()[0] == "colour_train_silver":
+		get_node("Sprite2D").texture = load("res://art_assets/silver_train_left.png")
+	elif get_groups()[0] == "colour_train_red":
+		get_node("Sprite2D").texture = load("res://art_assets/red_train_left.png")
+	elif get_groups()[0] == "colour_train_green":
+		get_node("Sprite2D").texture = load("res://art_assets/green_train_left.png")
+	elif get_groups()[0] == "colour_train_black":
+		get_node("Sprite2D").texture = load("res://art_assets/black_train_left.png")
+	elif get_groups()[0] == "colour_train_white":
+		get_node("Sprite2D").texture = load("res://art_assets/white_train_left.png")
+	elif get_groups()[0] == "colour_train_brown":
+		get_node("Sprite2D").texture = load("res://art_assets/brown_train_left.png")
 
 func _on_area_entered(area):
 	if area.is_in_group("spawn_up"):
